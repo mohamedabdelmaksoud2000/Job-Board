7599530c9e7c4de811ecd26b244ab95d1ee9d243
mohamed abdelmaksoud <ma0846301@gmail.com>
2025-03-28 16:25:14 +0200

    fix name model to language

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,4 +9,34 @@ class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
-}
+    protected $fillable = [
+        'title',
+        'description',
+        'company_name',
+        'salary_min',
+        'salary_max',
+        'is_remote',
+        'job_type',
+        'status',
+        'published_at',
+    ];
+    
+    protected $casts = [
+        'salary_min' => 'decimal:2',
+        'salary_max' => 'decimal:2',
+        'is_remote' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    
+    public function languages()
+    {
+        return $this->belongsToMany(Language::class);
+    }
+    
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+}   
